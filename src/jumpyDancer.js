55253dfcab17c65eb0f31a453bcb7a3bcb733145
andrewchen44 <andrewchen.c44@gmail.com>
2017-10-16 20:04:19 -0700

    Cleaned up code.

--- src/jumpyDancer.js
+++ src/jumpyDancer.js
@@ -1,14 +1,10 @@
 var makeJumpyDancer = function(top, left, timeBetweenSteps) {
-   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
   this.time = timeBetweenSteps;
-   var styleSettings = {
+  var styleSettings = {
     top: top,
     left: left
   };
-  //store top value
   this.top = top;
-  //store a boolean value as false
   this.bool = false;
   Dancer.call(this, top, left, timeBetweenSteps);
   this.$node = $('<span class="square"></span>');
@@ -22,17 +18,7 @@ makeJumpyDancer.prototype = Object.create(Dancer.prototype);
 makeJumpyDancer.prototype.constructor = makeJumpyDancer;
 
 makeJumpyDancer.prototype.step = function(timeBetweenSteps) {
-    // call the old version of step at the beginning of any call to this new version of step
   Dancer.prototype.step.call(this, this.time);
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-
-  
-  //if boolean is false
-    //increase the height of the dot using jquery
-  //decrease the height
-
   if (this.bool) {
     this.$node.css({top: this.top + 100});
     this.bool = false;
@@ -40,7 +26,4 @@ makeJumpyDancer.prototype.step = function(timeBetweenSteps) {
     this.$node.css({top: this.top - 100});
     this.bool = true;
   }
-
-
-  
 };
